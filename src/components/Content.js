e102d0536de87f467e0446d94f4ea905214800a8
ghgfhfg2 <46801546+ghgfhfg2@users.noreply.github.com>
2020-12-17 14:15:51 +0900

    1217

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Content() {
+  const [radio, setRadio] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(radio);
+  };
+  const onchange = (e) => {
+    setRadio(e.target.value);
+  };
   return (
     <>
       <div>
-        <input type="radio" name="lubchCehck" id="lunchType1" />
-        <label for="lunchType1">type1</label>
-        <input type="radio" name="lubchCehck" id="lunchType2" />
-        <label for="lunchType2">type2</label>
-        <input type="submit" value="확인" />
+        <form onSubmit={onSubmit}>
+          <input
+            type="radio"
+            name="lubchCehck"
+            id="lunchType1"
+            value="일반식"
+            cheched={radio === "일반식" ? true : false}
+            onChange={onchange}
+          />
+          <label htmlFor="lunchType1">type1</label>
+          <input
+            type="radio"
+            name="lubchCehck"
+            id="lunchType2"
+            value="다식"
+            cheched={radio === "다식" ? true : false}
+            onChange={onchange}
+          />
+          <label htmlFor="lunchType2">type2</label>
+          <input type="submit" value="확인" />
+        </form>
       </div>
     </>
   );
