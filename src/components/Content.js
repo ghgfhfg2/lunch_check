4c90068d7e20cfbde57714c8349217bd7ea30b33
ghgfhfg2 <46801546+ghgfhfg2@users.noreply.github.com>
2020-12-18 15:56:51 +0900

    1218

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,39 +1,52 @@
 import React, { useState } from "react";
 
 function Content() {
+  const [check, setCheck] = useState(false);
   const [radio, setRadio] = useState("");
+  const [checked, setChecked] = useState(false);
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(radio);
+    if (!check) {
+      alert("식단을 선택해 주세요");
+    } else {
+      setChecked(true);
+      console.log(checked);
+    }
   };
   const onchange = (e) => {
     setRadio(e.target.value);
+    setCheck(true);
   };
+  console.log(checked);
   return (
     <>
       <div>
         <form onSubmit={onSubmit}>
-          <div className="lunch-check-input">
-            <input
-              type="radio"
-              name="lubchCehck"
-              id="lunchType1"
-              value="일반식"
-              checked={radio === "일반식" ? true : false}
-              onChange={onchange}
-            />
-            <label htmlFor="lunchType1">type1</label>
-            <input
-              type="radio"
-              name="lubchCehck"
-              id="lunchType2"
-              value="다식"
-              checked={radio === "다식" ? true : false}
-              onChange={onchange}
-            />
-            <label htmlFor="lunchType2">type2</label>
-            <input type="submit" className="btn" value="확인" />
-          </div>
+          {checked === false ? (
+            <div className="lunch-check-input">
+              <input
+                type="radio"
+                name="lubchCehck"
+                id="lunchType1"
+                value="일반식"
+                checked={radio === "일반식" ? true : false}
+                onChange={onchange}
+              />
+              <label htmlFor="lunchType1">type1</label>
+              <input
+                type="radio"
+                name="lubchCehck"
+                id="lunchType2"
+                value="다식"
+                checked={radio === "다식" ? true : false}
+                onChange={onchange}
+              />
+              <label htmlFor="lunchType2">type2</label>
+              <input type="submit" className="btn" value="확인" />
+            </div>
+          ) : (
+            <div>checked</div>
+          )}
         </form>
       </div>
     </>
--- src/components/Mypage.js
+++ src/components/Mypage.js
@@ -5,8 +5,18 @@ function Mypage() {
     <>
       <ul className="myinfo">
         <li>
-          <span className="date">2020.12.17(목)</span>
-          <span className="choice">type1</span>
+          <div>
+            <span className="date">2020.12.17(목)</span>
+            <span className="choice">type1</span>
+          </div>
+          <p>내용</p>
+        </li>
+        <li>
+          <div>
+            <span className="date">2020.12.17(목)</span>
+            <span className="choice">type1</span>
+          </div>
+          <p>내용</p>
         </li>
       </ul>
     </>
